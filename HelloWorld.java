df131e8f5a6a1fe26738d1fe0187754b33dc22ef
ankitnimbolkar-clg <adnimbolkar_m24@ce.vjti.ac.in>
2025-01-27 21:17:49 +0530

    Made changes in File

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,5 @@
 class HelloWorld{
     public static void main(String[] args) {
-        System.out.println("Hey, Github!!");
-        System.out.println("Ankit");
+        System.out.println("Hello, Github!!");
     }
 }
